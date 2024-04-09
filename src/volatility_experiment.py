a6552fa4d8fb351712c9e1e9e8fd6ee8c32acf95
Tabaktom <tabaktom360@gmail.com>
2024-04-09 18:30:04 +0300

    starting strategy

--- src/volatility_experiment.py
+++ src/volatility_experiment.py
@@ -7,26 +7,33 @@ from src.utils import *
 warnings.filterwarnings("ignore")
 
 
+def get_exp_date_to_strike_dist_from_underlying(df):
+    df = format_column_names(df)
+    df = df[df["QUOTE_DATE"] <= "2019-07-29"]
+    df = df[df["DTE"] <= 14]
+    unique_exp_dates = df["EXPIRE_DATE"].unique()
+
+    exp_date_to_strike_dist_from_underlying = defaultdict(defaultdict)
+    for i in unique_exp_dates:
+        sub_df = df[df["EXPIRE_DATE"] == i]
+        unique_strikes = sub_df["STRIKE"].unique()
+        for s in unique_strikes:
+            s_df = sub_df[sub_df["STRIKE"] == s]
+            max_dte = s_df["DTE"].max()
+            max_dte_df = s_df[s_df["DTE"] == max_dte]
+            dist_from_underlying = (max_dte_df["STRIKE"].values - max_dte_df["UNDERLYING_LAST"].values) / max_dte_df[
+                "UNDERLYING_LAST"].values
+
+            exp_date_to_strike_dist_from_underlying[str(i)][str(s)] = dist_from_underlying[0]
+
+    return exp_date_to_strike_dist_from_underlying, df
 
 
 def main(path, rfr_path, out_dir, vis=False):
     df = pd.read_csv(path)
-    df = format_column_names(df)
-    df = df[df["QUOTE_DATE"]<="2019-07-29"]
-    df = df[df["DTE"]<=14]
-    unique_exp_dates = df["EXPIRE_DATE"].unique()
-    option_pricer = OptionPricer(rfr_path)
-    exp_date_to_strike_dist_from_underlying = defaultdict(defaultdict)
-    for i in unique_exp_dates:
-        sub_df = df[df["EXPIRE_DATE"]==i]
-        unique_strikes = sub_df["STRIKE"].unique()
-        for s in unique_strikes:
-            s_df = sub_df[sub_df["STRIKE"]==s]
-            max_dte = s_df["DTE"].max()
-            max_dte_df = s_df[s_df["DTE"]==max_dte]
-            dist_from_underlying = (max_dte_df["STRIKE"].values - max_dte_df["UNDERLYING_LAST"].values)/max_dte_df["UNDERLYING_LAST"].values
+    exp_date_to_strike_dist_from_underlying, df = get_exp_date_to_strike_dist_from_underlying(df)
 
-            exp_date_to_strike_dist_from_underlying[str(i)][str(s)] = dist_from_underlying[0]
+    option_pricer = OptionPricer(rfr_path)
 
     desired_levels = [-0.25, -0.1, 0, 0.1, 0.25]
     chosen_exp_date_strike_levels = get_exp_date_strike_at_desried_levels(exp_date_to_strike_dist_from_underlying, desired_levels)
@@ -135,7 +142,7 @@ def main(path, rfr_path, out_dir, vis=False):
                                                       put_plot_df.columns, underlying_plot_df.columns):
             path = os.path.join(iv_prem_out_dir, f"Expiry_{under_col}.png")
             at_money_strike_price = float(under_col.split("AT_")[-1])
-            fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4, sharex=True)
+            fig, (ax0, ax1, ax2, ax3) = plt.subplots(nrows=4, sharex=True, figsize=(25, 15))
             # sent_col = sentiment_plot_df.columns[0]
             # call_col = call_plot_df.columns[0]
             # put_col = put_plot_df.columns[0]
@@ -162,6 +169,7 @@ def main(path, rfr_path, out_dir, vis=False):
             put_plot_df.sort_index(ascending=False)[put_col].ffill().plot(ax=ax3)
             ax3.title.set_text(f'Put Volatility Premium (T-14 at {put_col})')
             ax3.axhline(y=0, c="black")
+            ax3.set_xlabel("Days to Expiry")
             # ax3.set_xticks(put_plot_df.sort_index(ascending=False).index.values)
             ax3.invert_xaxis()
             plt.tight_layout()
@@ -184,16 +192,87 @@ def main(path, rfr_path, out_dir, vis=False):
         dte_iv_smile_upper = dte_iv_smile_matrix_mean + dte_iv_smile_matrix_std
         dte_iv_smile_lower = dte_iv_smile_matrix_mean - dte_iv_smile_matrix_std
 
-        fig = plt.Figure()
+        fig = plt.Figure(figsize=(5, 5))
         plt.plot(desired_levels, dte_iv_smile_matrix_mean, label="Mean IV")
         plt.fill_between(desired_levels, dte_iv_smile_upper, dte_iv_smile_lower, alpha=0.2, label="Mean IV +- 1std")
         plt.xlabel("Option Strike Price Distance from Underlying Price at T-14")
         plt.ylabel("Implied Volatility")
         plt.title(f"Implied Volatility Smile at Days to Expiry={dte}")
         plt.legend()
+        plt.tight_layout()
 
         plt.savefig(file_path)
         plt.clf()
+
+
+def calculate_strategy_performance(path):
+    df = pd.read_csv(path)
+    exp_date_to_strike_dist_from_underlying, df = get_exp_date_to_strike_dist_from_underlying(df)
+    distances_from_underlying_spot = [0.1, 0.25]
+    relevant_cols = ["UNDERLYING_LAST", "C_IV", "P_IV", "C_BID", "C_ASK", "P_BID", "P_ASK", "DTE"]
+    for distance_level in distances_from_underlying_spot:
+        desired_levels = [-distance_level, 0, distance_level]
+        chosen_exp_date_strike_levels = get_exp_date_strike_at_desried_levels(exp_date_to_strike_dist_from_underlying,
+                                                                              desired_levels)
+
+        chosen_exp_date_strike_levels["EXPIRE_DATE"] = pd.to_datetime(chosen_exp_date_strike_levels["EXPIRE_DATE"])
+        chosen_exp_date_strike_levels["STRIKE"] = chosen_exp_date_strike_levels["STRIKE"].astype(float)
+        unique_expiry = chosen_exp_date_strike_levels["EXPIRE_DATE"].unique()
+
+        for expiry_date in unique_expiry:
+            contract_data = {"Put_OutOfMoney": None, "AtMoney": None, "Call_OutofMoney": None}
+            contract_names = list(contract_data.keys())
+            expiry_date_data = df[df["EXPIRE_DATE"]==expiry_date]
+            strike_prices = chosen_exp_date_strike_levels["STRIKE"][chosen_exp_date_strike_levels["EXPIRE_DATE"]==expiry_date]
+            strike_prices = strike_prices.sort_values(ascending=True)
+
+            for ind, s in enumerate(strike_prices):
+                level_name = contract_names[ind]
+                strike_data = expiry_date_data[expiry_date_data["STRIKE"]==s]
+                strike_data = strike_data.set_index("QUOTE_DATE")
+                strike_data = strike_data[relevant_cols]
+                contract_data[level_name] = strike_data
+
+            contract_df = get_contract_df(strike_data, contract_data)
+            _=0
+
+
+
+    return
+
+
+def get_contract_df(strike_data, contract_data):
+    contract_df = pd.DataFrame(index=strike_data.index)
+    contract_df["DTE"] = contract_data["AtMoney"]["DTE"]
+    contract_df["UNDERLYING_LAST"] = contract_data["AtMoney"]["UNDERLYING_LAST"]
+    contract_df["AT_MONEY_IV"] = contract_data["AtMoney"]["C_IV"]
+
+    contract_df["CALL_BUY"] = contract_data["Call_OutofMoney"]["C_ASK"]
+    contract_df["CALL_SELL"] = contract_data["Call_OutofMoney"]["C_BID"]
+    contract_df["CALL_IV"] = contract_data["Call_OutofMoney"]["C_IV"]
+
+    contract_df["PUT_BUY"] = contract_data["Put_OutOfMoney"]["P_ASK"]
+    contract_df["PUT_SELL"] = contract_data["Put_OutOfMoney"]["P_BID"]
+    contract_df["PUT_IV"] = contract_data["Put_OutOfMoney"]["P_IV"]
+
+    contract_df["CALL_IV_PREM"] = contract_df["CALL_IV"] - contract_df["AT_MONEY_IV"]
+    contract_df["PUT_IV_PREM"] = contract_df["PUT_IV"] - contract_df["AT_MONEY_IV"]
+    contract_df["SENTIMENT_IV_PREM"] = contract_df["CALL_IV_PREM"] - contract_df["PUT_IV_PREM"]
+
+    contract_df = contract_df.sort_index(ascending=True)
+    return contract_df
+
+
+def run_strategy(contract_df):
+    equity = None
+    trades_df = pd.DataFrame(columns=["DayEnter", "DayExit", "Premium_Amount", "ExpiryAmount", "Profit/Loss"])
+
+    position = None
+    for ind, row in contract_df.iterrows():
+        pass
+
+
+    return equity
 
 
 
@@ -204,5 +283,6 @@ if __name__ == "__main__":
            "Data/aapl_2016_2020.csv"
     rfr_path = "/Users/tom/Desktop/MBA/SemesterA/InvestmentTheory/Project/Data/DGS10.csv"
     out_dir = "/Volumes/Elements/Options_study/AAPL/"
-    main(path, rfr_path, out_dir, vis=True)
+    # main(path, rfr_path, out_dir, vis=False)
+    calculate_strategy_performance(path)
 
